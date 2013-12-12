ee9d1bbff94376c76ba01f5513e4782c37cc045a
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-12-12 09:38:27 -0200

    christmas list example

---
+++ christmas-list/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+  "github.com/codegangsta/martini"
+  "github.com/codegangsta/martini-contrib/render"
+  "github.com/codegangsta/martini-contrib/binding"
+  "labix.org/v2/mgo"
+)
+
+type Wish struct {
+  Name        string `form:"name"`
+  Description string `form:"description"`
+}
+
+func DB() martini.Handler {
+  session, err := mgo.Dial("mongodb://localhost")
+  if err != nil {
+    panic(err)
+  }
+
+  return func(c martini.Context) {
+    s := session.Clone()
+    c.Map(s.DB("advent"))
+    defer s.Close()
+    c.Next()
+  }
+}
+
+func GetAll(db *mgo.Database) []Wish {
+  var wishlist []Wish
+  db.C("wishes").Find(nil).All(&wishlist)
+  return wishlist
+}
+
+func main() {
+  m := martini.Classic()
+  m.Use(render.Renderer())
+  m.Use(DB())
+
+  m.Get("/", func() string {
+      return "Merry Christmas!"
+  })
+
+  m.Get("/wishes", func(r render.Render, db *mgo.Database) {
+    r.HTML(200, "list", GetAll(db))
+  })
+
+  m.Post("/wishes", binding.Form(Wish{}), func(wish Wish, r render.Render,
+    db *mgo.Database) {
+    db.C("wishes").Insert(wish)
+    r.HTML(200, "list", GetAll(db))
+  })
+
+  m.Run();
+}
